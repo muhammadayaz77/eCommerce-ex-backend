ea7db11853a12e5a923e946ffcc2648d2ffbdfce
Muhammad Ayaz <muhammadayaz22757@gmail.com>
2024-10-04 11:24:56 +0500

    add GET Method

--- index.mjs
+++ index.mjs
@@ -1,6 +1,6 @@
 import express from 'express'
 import cors from 'cors'
-
+import { DEFAUL_ITEMS } from './data/items.mjs';
 let app = express();
 let PORT = 3000;
 
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/api/data',(req,res) => {
-  res.status(200).json({message : "Data is Fetched" })
+  res.status(200).json(DEFAUL_ITEMS)
 })
 
 app.listen(PORT,() => {
